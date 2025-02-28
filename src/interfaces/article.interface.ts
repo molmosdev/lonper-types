9d3b51355cf124681afde7581332b16e2e22cda4
molmosdev <hello@molmos.dev>
2025-02-28 19:30:36 +0100

    Add detailed properties to IArticle interface for comprehensive data representation

--- src/interfaces/article.interface.ts
+++ src/interfaces/article.interface.ts
@@ -1,1 +1,24 @@
-export interface IArticle {}
+export interface IArticle {
+  id: string;
+  article: string;
+  name: string;
+  familyCode: string;
+  familyDesc: string;
+  subfamilyCode: string;
+  subfamilyDesc: string;
+  blqC: number;
+  blqV: number;
+  type: string;
+  subtype: string;
+  shortName: string | null;
+  blqVName: string | null;
+  lonRate: string;
+  lonGroup: string;
+  lonEdging: string;
+  salePrice: number;
+  unsubscribed: number;
+  crtlStk: number;
+  dto1: number;
+  dto2: number;
+  dto3: number;
+}
