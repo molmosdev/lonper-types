e4f90b58738bd193fcf193b90a9f322bf522e395
molmosdev <hello@molmos.dev>
2025-06-02 22:09:47 +0200

    Fix import statement formatting in request.interface.ts

--- src/interfaces/request.interface.ts
+++ src/interfaces/request.interface.ts
@@ -1,11 +1,13 @@
-import { IRequestSummary } from './request-resume.interface';
-import { IRequestArticle } from './request-article.interface';
-import { RequestType } from '../enums/request-type.enum';
-import { IRequestAddress } from './request-address.interface';
+import { IRequestSummary } from "./request-resume.interface";
+import { IRequestArticle } from "./request-article.interface";
+import { RequestType } from "../enums/request-type.enum";
+import { IRequestAddress } from "./request-address.interface";
 
 export interface IRequest {
+  id?: string;
   address: IRequestAddress;
   articles: IRequestArticle[];
+  clientName: string;
   clientNumber: number;
   reference: string;
   deliveryDate: Date | undefined;
