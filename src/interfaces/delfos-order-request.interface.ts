849bb1e31e1535777a2cff669342b19ed9d68c56
molmosdev <hello@molmos.dev>
2025-07-29 01:28:45 +0200

    Bump version to 0.0.23 in package.json and update deliveryDate property to allow undefined in IDelfosOrderRequest interface

--- src/interfaces/delfos-order-request.interface.ts
+++ src/interfaces/delfos-order-request.interface.ts
@@ -4,7 +4,7 @@ import { IDelfosOrderArticle } from "./delfos-order-article.interface";
 export interface IDelfosOrderRequest {
   id: string;
   reference: string;
-  deliveryDate: Date;
+  deliveryDate: Date | undefined;
   client: number;
   address: number;
   articles: IDelfosOrderArticle[];
